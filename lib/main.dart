74e5f9338e363527d51b2a8f6bf8163560c0c260
jpssantiago <jps_santiago@outlook.com>
2021-03-27 19:11:44 -0300

    Uploading initial files.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Center(
+            child: Text('Hello, world!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
