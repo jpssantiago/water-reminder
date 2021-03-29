44e4bdbf93aff640fd7509340e8a9116acc7ea9a
jpssantiago <jps_santiago@outlook.com>
2021-03-29 14:47:16 -0300

    Added first version of user settings page.

--- lib/pages/user_settings/user_settings_page.dart
+++ lib/pages/user_settings/user_settings_page.dart
@@ -1,15 +1,44 @@
-import 'package:app/widgets/white_app_bar.dart';
+import 'package:app/localization/app_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:app/providers/user_provider.dart';
+import 'package:app/widgets/white_app_bar.dart';
 
 class UserSettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildWhiteAppBar(),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Text('Settings'),
+    return Consumer<UserProvider>(
+      builder: (context, provider, child) => Scaffold(
+        appBar: buildWhiteAppBar(
+          title: AppLocalizations.of(context).translate('settings'),
+        ),
+        body: Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  AppLocalizations.of(context).translate('edit_user'),
+                ),
+                trailing: RotatedBox(
+                  quarterTurns: 1,
+                  child: Icon(Icons.navigation),
+                ),
+                enableFeedback: true,
+              ),
+              ListTile(
+                leading: Icon(Icons.local_drink),
+                title: Text(
+                  AppLocalizations.of(context).translate('edit_drink_goal'),
+                ),
+                trailing: Text(
+                  '${provider.user.drinkGoal.toStringAsFixed(0)} ml',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/white_app_bar.dart
+++ lib/widgets/white_app_bar.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-AppBar buildWhiteAppBar() {
+AppBar buildWhiteAppBar({String title}) {
   return AppBar(
     backgroundColor: Colors.white,
     elevation: 0,
     iconTheme: IconThemeData(color: Colors.black),
+    title: title != null
+        ? Text(
+            title,
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        : null,
   );
 }
